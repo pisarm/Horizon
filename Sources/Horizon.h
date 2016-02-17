8c2485fd3f90fcab6141ff0a4551c8863b9092d9
pisarm <flemming@pisarm.dk>
2016-02-17 21:24:35 +0100

    Tweaked project settings

--- Sources/Horizon.h
+++ Sources/Horizon.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 pisarm.dk. All rights reserved.
 //
 
-@import Foundation;
+#import <Foundation/Foundation.h>
 
 FOUNDATION_EXPORT double HorizonVersionNumber;
 FOUNDATION_EXPORT const unsigned char HorizonVersionString[];
